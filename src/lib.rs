63aef712c3a5e107e287679a23641271f2e30311
Your Name <you@example.com>
2026-03-09 19:40:42 +0900

    責務分離を実施

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,17 @@
 use bevy::ecs::component::Component;
+use bevy::{DefaultPlugins, app::{App, Startup}};
+use burn_cuda::{Cuda};
+use crate::inspection::gpu::setup;
 
 pub mod inspection;
 #[derive(Component)]
 pub struct Entity{
     pub math:u32
+}
+
+impl Entity {
+    pub fn handle(){
+        type Backend = Cuda;
+        App::new().add_plugins(DefaultPlugins).add_systems(Startup, setup::<Backend>).run();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use bevy::{DefaultPlugins, app::{App, Startup}};
-use nn::inspection::{gpu::setup};
-use burn_cuda::{Cuda};
+use nn::Entity;
+
+
 fn main() {
-    type Backend = Cuda;
-    App::new().add_plugins(DefaultPlugins).add_systems(Startup, setup::<Backend>).run();
+    Entity::handle();
 }
